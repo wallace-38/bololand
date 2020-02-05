e8802d9ad33270928931b0ea5644c066a1270519
wallace-38 <senac2019.1981@gmail.com>
2020-02-05 15:36:54 -0300

    atualização

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+include_once("config.php");
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 
@@ -33,195 +37,42 @@
                         <a class="nav-link" href="index.php">Inicio</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#produtos">Produtos</a>
+                        <a class="nav-link" href="index.php#produtos">Produtos</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#clientes">Clientes</a>
+                        <a class="nav-link" href="index.php#clientes">Clientes</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#lojas">Lojas</a>
+                        <a class="nav-link" href="index.php#lojas">Lojas</a>
                     </li>
                 </ul>
                 <ul class="navbar-nav ml-auto">
                     <li class="nav-item">
-                        <a class="nav-link" href="index.php?p=cadastro">Cadastro</a>
+                        <a class="nav-link" href="index.php?pag=cad">Cadastro</a>
                     </li>
 
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Entrar</a>
+                        <a class="nav-link" href="index.php?pag=log">Entrar</a>
                     </li>
                 </ul>
             </div>
         </nav>
 
         <?php
-        if (isset($_GET["p"])) {
-            if ($_GET["p"] == "cadastro") {
-                include("formUser.php");
+        if (!empty($_GET)) {
+            if ($_GET["pag"] == "cad") {
+                include("pages/formUser.php");
+            } else if ($_GET["pag"] == "log") {
+                include("pages/login.php");
+            } else {
+                include("pages/inicio.php");
             }
+        } else {
+            include("pages/inicio.php");
         }
         ?>
 
-        <section class="container bg-branco" id="produtos">
-            <div class="row">
-                <div class="col-12 col-md-4 itens">
-                    <img src="img/produtos/produto01.jpg" class="img-fluid" alt="">
-                    <h2>Titulo</h2>
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestias fugiat sit error earum hic
-                        vel perferendis ad distinctio, voluptatem sint. Assumenda deserunt tempora reiciendis quibusdam
-                        dolores accusantium qui ab vel?</p>
-                </div>
-                <div class="col-12 col-md-4 itens">
-                    <img src="img/produtos/produto02.jpg" class="img-fluid" alt="">
-                    <h2>Titulo</h2>
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestias fugiat sit error earum hic
-                        vel perferendis ad distinctio, voluptatem sint. Assumenda deserunt tempora reiciendis quibusdam
-                        dolores accusantium qui ab vel?</p>
-                </div>
-                <div class="col-12 col-md-4 itens">
-                    <img src="img/produtos/produto03.jpg" class="img-fluid" alt="">
-                    <h2>Titulo</h2>
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestias fugiat sit error earum hic
-                        vel perferendis ad distinctio, voluptatem sint. Assumenda deserunt tempora reiciendis quibusdam
-                        dolores accusantium qui ab vel?</p>
-                </div>
-            </div>
-        </section>
-
-        <div class="container bg-branco" id="propag-1">
-            <div class="row">
-                <img src="img/propaganda/p200x1000-01-2.jpg" class="col-12 p-0" alt="Propaganda">
-            </div>
-        </div>
-
-        <section class="container bg-branco" id="clientes">
-            <div class="row">
-                <div class="col-md-8">
-                    <div class="row comentario">
-                        <div class="col-md-4 foto">
-                            <img src="img/clientes/cliente1.jpg" class="rounded-circle" alt="">
-                        </div>
-                        <div class="col-md-8 texto">
-                            <h2>Homer Simpson</h2>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur autem iusto ab
-                                nobis possimus expedita molestias! Obcaecati impedit consectetur cumque repellendus
-                                repudiandae similique modi laborum magni atque, deleniti odio laboriosam.</p>
-                        </div>
-                    </div>
-
-                    <div class="row comentario">
-                        <div class="col-md-4 foto">
-                            <img src="img/clientes/cliente2.png" class="rounded-circle" alt="">
-                        </div>
-                        <div class="col-md-8 texto">
-                            <h2>Bob Esponja</h2>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur autem iusto ab
-                                nobis possimus expedita molestias! Obcaecati impedit consectetur cumque repellendus
-                                repudiandae similique modi laborum magni atque, deleniti odio laboriosam.</p>
-                        </div>
-                    </div>
-
-                    <div class="row comentario">
-                        <div class="col-md-4 foto">
-                            <img src="img/clientes/cliente3.png" class="rounded-circle" alt="">
-                        </div>
-                        <div class="col-md-8 texto">
-                            <h2>Patrick Estrela</h2>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur autem iusto ab
-                                nobis possimus expedita molestias! Obcaecati impedit consectetur cumque repellendus
-                                repudiandae similique modi laborum magni atque, deleniti odio laboriosam.</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-4 text-center">
-                    <img src="img/propaganda/p300x800-01.jpg" class="img-fluid" alt="" id="propag-2">
-                </div>
-            </div>
-        </section>
-
-        <section class="p-0">
-            <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
-                <!-- <ol class="carousel-indicators">
-                    <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
-                    <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
-                    <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
-                </ol> -->
-                <div class="carousel-inner">
-                    <div class="carousel-item active">
-                        <img src="img/produtos/p1.jpg" class="d-block w-100" alt="...">
-                        <div class="texto-carrosel branco">
-                            <h5>First slide label</h5>
-                            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                        </div>
-                    </div>
-                    <div class="carousel-item">
-                        <img src="img/produtos/p2.jpg" class="d-block w-100" alt="...">
-                        <div class="texto-carrosel branco">
-                            <h5>Second slide label</h5>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                        </div>
-                    </div>
-                    <div class="carousel-item">
-                        <img src="img/produtos/p3.jpg" class="d-block w-100" alt="...">
-                        <div class="texto-carrosel branco">
-                            <h5>Third slide label</h5>
-                            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                        </div>
-                    </div>
-                </div>
-                <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Previous</span>
-                </a>
-                <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Next</span>
-                </a>
-            </div>
-        </section>
-
-        <section class="container bg-branco" id="lojas">
-            <div class="row">
-                <div class="col-md-6 px-5">
-                    <form action="">
-                        <div class="form-group">
-                            <label for="examplenome">Nome</label>
-                            <input type="text" class="form-control" id="examplenome" placeholder="Nome completo">
-                        </div>
-
-                        <div class="form-group">
-                            <label for="exampleemail">E-mail</label>
-                            <input type="email" class="form-control" id="exampleemail" placeholder="nome@email.com">
-                        </div>
-
-                        <div class="form-group">
-                            <label for="exampleselect">Assunto</label>
-                            <select class="form-control" id="exampleselect">
-                                <option value="1">Elogio</option>
-                                <option value="2">Sugentões</option>
-                                <option value="3">Reclamações</option>
-                            </select>
-                        </div>
-
-                        <div class="form-group">
-                            <label for="exampleTextarea">Contexto</label>
-                            <textarea class="form-control" id="exampleTextarea" rows="5"></textarea>
-                        </div>
-
-                        <div class="form-group text-right">
-                            <button type="button" class="btn btn-primary">Enviar</button>
-                        </div>
-                    </form>
-                </div>
-                <div class="col-md-6 px-5">
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d14701.041363083958!2d-43.55107225!3d-22.90376645!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1spt-BR!2sbr!4v1580494472868!5m2!1spt-BR!2sbr" width="98%" height="300" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
-                    <h3 class="py-2">Matriz</h3>
-                    <p>R. Ferreira Borges, 58 - Campo Grande, Rio de Janeiro - RJ, 23050-350</p>
-                </div>
-            </div>
-        </section>
-
-        <footer class="bg-azul branco py-5">
+        <footer class="bg-azul branco container-fluid py-5">
             <div class="row">
                 <div class="col-md-3">
                     <ul>
